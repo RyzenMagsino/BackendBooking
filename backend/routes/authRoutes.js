5df330395fe651ec1eb890b0e176781a745738d1
Wewell29 <22-19564@g.batstate-u.edu.ph>
2025-07-15 15:17:26 +0800

    W4-D2 Backend for login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controllers/authController');
+const { register, login } = require('../controllers/authController');
 
-// Signup route
+// Signup
 router.post('/signup', register);
+
+// Login
+router.post('/login', login);
 
 module.exports = router;
